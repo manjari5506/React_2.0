2cf115178023bda686c279a779771175375d6434
Manjari Raikwar <raikwar.manjari@gmail.com>
2022-08-21 22:40:23 +0530

    Delete Rct-211-e1 directory

--- Rct-211-e1/src/Redux/actionTypes.js
+++
@@ -1,9 +0,0 @@
-
-
-export const GET_SHOES_REQUEST = "getShoesRequest";
-export const GET_SHOES_SUCCESS = "getShoesSuccess";
-export const GET_SHOES_FAILURE = "getShoesFailure";
-
-export const UPDATE_SHOE_COUNT_REQUEST = "updateShoeCountRequest";
-export const UPDATE_SHOE_COUNT_SUCCESS = "updateShoeCountSuccess";
-export const UPDATE_SHOE_COUNT_FAILURE = "updateShoeCountFailure";
